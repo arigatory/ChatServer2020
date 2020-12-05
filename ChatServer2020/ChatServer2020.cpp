c313f8cda2ff7d4c4a644c46872d0895ab71d09f
Иван Иван <remillius_dies@outlook.com>
2020-12-05 21:05:55 +0300

    sent

--- ChatServer2020/ChatServer2020.cpp
+++ ChatServer2020/ChatServer2020.cpp
@@ -3,6 +3,9 @@
 #include <regex>
 
 using namespace std;
+
+
+
 const unsigned long FIRST_ID = 10;
 const unsigned long BOT_ID = 1;
 
@@ -16,6 +19,7 @@ struct UserConnection
 // SET_NAME=Mike
 // MESSAGE_TO=11,Hello to you, mr Ivan
 // MESSAGE_TO=10,Hello to you, dear Mile
+// NEW_USER=19,Mike
 
 
 //TASK 2,3
@@ -43,18 +47,21 @@ int main()
     int port = 8888;
     unsigned long latest_user_id = FIRST_ID;
     unsigned long total_connected = 0;
+    map<unsigned long, string> online_users;
+
     uWS::App().ws<UserConnection>("/*", {
-        .open = [&latest_user_id, &total_connected](auto * ws) {
+        .open = [&latest_user_id, &total_connected, &online_users](auto * ws) {
             UserConnection* data = (UserConnection*) ws->getUserData();
             data->user_id = latest_user_id++;
             data->name = "UNNAMED";
             cout << "New user connected ID = " << data->user_id << endl;
+            online_users[data->user_id] = data->name;
             ws->subscribe("broadcast");
             ws->subscribe("user#" + to_string(data->user_id));
             //TASK 1
-            cout << "Total user connected: " << ++total_connected << endl;
+            cout << "Total user connected: " << ++ total_connected << endl;
         },
-        .message = [&latest_user_id](auto* ws, std::string_view message, uWS::OpCode opCode) {
+        .message = [&latest_user_id, &online_users](auto* ws, std::string_view message, uWS::OpCode opCode) {
             UserConnection* data = (UserConnection*)ws->getUserData();
             const auto& input = string(message);
 
@@ -74,13 +81,14 @@ int main()
                     data->name = new_name;
                     ws->publish("broadcast", "Welcome: " + new_name);
                     cout << "User" << user_id << " sets their name to " << new_name << endl;
+                    ws->publish("broadcast", "NEW_USER = " + user_id + "," + data->name);
+                    online_users[data->user_id] = data->name;
                 }
                 else
                 {
                     cout << "User" << user_id << " tried to use unallowed name: " << new_name << endl;
                     ws->publish("user#" + user_id, "Sorry, you can't use this name, try different one!");
                 }
-                
             }
             if (regex_match(input, match_result, MESSAGE_TO))
             {
