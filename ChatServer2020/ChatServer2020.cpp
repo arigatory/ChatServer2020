e939e97ba934faceb0d8e0d87515325a30a4553d
Иван Иван <remillius_dies@outlook.com>
2020-12-05 19:37:18 +0300

    task 1-4

--- ChatServer2020/ChatServer2020.cpp
+++ ChatServer2020/ChatServer2020.cpp
@@ -3,6 +3,8 @@
 #include <regex>
 
 using namespace std;
+const unsigned long FIRST_ID = 10;
+const unsigned long BOT_ID = 1;
 
 
 struct UserConnection
@@ -16,38 +18,86 @@ struct UserConnection
 // MESSAGE_TO=10,Hello to you, dear Mile
 
 
+//TASK 2,3
+bool good_name(string name)
+{
+    if (name.find(",") != string::npos) {
+        cout << name.find(",") << name << endl;
+        return false;
+    }
+    if (name.length() > 255) {
+        cout << "255" << endl;
+        return false;
+    }
+    return true;
+}
+
+//TASK4
+bool exist_id(unsigned long latest_id, unsigned long id) {
+    return (id < latest_id) and (id >= FIRST_ID) or (id == BOT_ID);
+}
+
+
 int main()
 {
     int port = 8888;
-    
-    unsigned long latest_user_id = 10;
+    unsigned long latest_user_id = FIRST_ID;
+    unsigned long total_connected = 0;
     uWS::App().ws<UserConnection>("/*", {
-        .open = [&latest_user_id](auto * ws) {
+        .open = [&latest_user_id, &total_connected](auto * ws) {
             UserConnection* data = (UserConnection*) ws->getUserData();
             data->user_id = latest_user_id++;
             data->name = "UNNAMED";
-            cout << "New user connected ID =  << " << data->user_id << endl;
+            cout << "New user connected ID = " << data->user_id << endl;
             ws->subscribe("broadcast");
-            ws->subscribe("user#" + data->user_id);
+            ws->subscribe("user#" + to_string(data->user_id));
+            //TASK 1
+            cout << "Total user connected: " << ++total_connected << endl;
         },
-        .message = [](auto* ws, std::string_view message, uWS::OpCode opCode) {
-            string SET_NAME("SET_NAME=");
-            string MESSAGE_TO("MESSAGE_TO=");
+        .message = [&latest_user_id](auto* ws, std::string_view message, uWS::OpCode opCode) {
             UserConnection* data = (UserConnection*)ws->getUserData();
-            cout << "New message received = " << message << endl;
-            if (message.find(SET_NAME)==0)
+            const auto& input = string(message);
+
+            const regex SET_NAME("(SET_NAME)=(.*)");
+            const regex MESSAGE_TO("(MESSAGE_TO)=(\\d+),(.*)");
+
+            smatch match_result;
+
+            cout << "New message received = " << input << endl;
+       
+            if (regex_match(input,match_result, SET_NAME))
             {
-                cout << "User sets their name" << endl;
-                data->name = message.substr(SET_NAME.length());
+                string new_name = match_result[2].str();
+                string user_id = to_string(data->user_id);
+                if (good_name(new_name))
+                {
+                    data->name = new_name;
+                    ws->publish("broadcast", "Welcome: " + new_name);
+                    cout << "User" << user_id << " sets their name to " << new_name << endl;
+                }
+                else
+                {
+                    cout << "User" << user_id << " tried to use unallowed name: " << new_name << endl;
+                    ws->publish("user#" + user_id, "Sorry, you can't use this name, try different one!");
+                }
+                
             }
-            if (message.find("MESSAGE_TO=") == 0)
+            if (regex_match(input, match_result, MESSAGE_TO))
             {
-                cout << "User sends private message" << endl;
-                auto rest = message.substr(MESSAGE_TO.length());
-                int comma_position = rest.find(",");
-                auto ID = rest.substr(0, comma_position);
-                auto text = rest.substr(comma_position+1);
-                ws->publish("user#" + string(ID), text);
+                string id_to = match_result[2].str();
+                string message_to_send = match_result[3].str();
+                string user_id = to_string(data->user_id);
+                if (exist_id(latest_user_id,std::stoul(id_to)))
+                {
+                    ws->publish("user#" + id_to, message_to_send);
+                    cout << "User" << user_id << " seends private message to User" << id_to << endl;
+                }
+                else
+                {
+                    ws->publish("user#" + user_id, "Error, there is no user with ID = " + id_to);
+                    cout << "User" << data->user_id << " tried to send message to unexisting ID = " << id_to << endl;
+                }
+                
             }
         }
         }).listen(port, [port](auto* token) {
